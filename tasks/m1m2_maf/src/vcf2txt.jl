fd0d3be9c36aeac5254d258e754082621b44b26f
Chip_Stewart <donald.a.stewart@gmail.com>
2017-02-27 16:28:50 -0500

    blank lines fix

--- tasks/m1m2_maf/src/vcf2txt.jl
+++ tasks/m1m2_maf/src/vcf2txt.jl
@@ -7,22 +7,8 @@ using jlVCF
 narg = 0
 f=STDOUT
 max_events=2^31
-#vcf_file1="/Volumes/64GB_2015/Work/REBC/sample.broad-mutect.DATECODE.somatic.snv_mnv.vcf"
 vcf_file1=ARGS[1]
 
-# is this a gz file? 
-# f=splitext(vcf_file1)
-# if (uppercase(f[2])==".GZ")
-#    f0=vcf_file1
-#    f1=splitdir(f[1])
-#    f1=f[1]
-#    f2=splitdir(f1)
-#    f2=f2[2]
-#    ;zcat $f0 > $f2
-#    #run(`zcat' $f0  |> $f2')
-#     #run(pipeline(`zcat $f0`, stdout=$f2, stderr="zcat.stderr.log"))
-#    vcf_file1=f2
-# end
 
 narg=length(ARGS)
 #println(narg)
@@ -87,6 +73,9 @@ count = 0
 v = "dmy"
 while !eof(vc)
     v = next1(vc)
+    if isa(v,Bool)
+    	continue
+    end
     if sizeof(v)<2
     	continue
     end
