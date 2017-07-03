5aa604f255b0c64d742422a4d1090d086373618f
Chip_Stewart <donald.a.stewart@gmail.com>
2017-07-03 14:46:14 -0400

    changed name

--- tasks/M1M2Strelka_maf/src/M1M2Strelka_maf.sh
+++ tasks/M1M2Strelka_maf/src/M1M2Strelka_maf.sh
@@ -9,6 +9,6 @@ julia /opt/src/vcf2txt.jl $VCF1  tmp1.tsv
 julia /opt/src/vcf2txt.jl $VCF2  tmp2.tsv
 julia /opt/src/vcf2txt.jl $VCF3  tmp3.tsv
 julia /opt/src/vcf2txt.jl $VCF4  tmp4.tsv
-julia /opt/src/StrelkaM1M2_maflite.jl $TID $NID tmp1.tsv tmp2.tsv tmp3.tsv tmp4.tsv m1m2strelka_maflite.tsv
+julia /opt/src/M1M2Strelka_maflite.jl $TID $NID tmp1.tsv tmp2.tsv tmp3.tsv tmp4.tsv m1m2strelka_maflite.tsv
 
 
