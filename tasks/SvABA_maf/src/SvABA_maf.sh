c937bfa45b093307248e6cead05a2462042ccba5
Chip_Stewart <donald.a.stewart@gmail.com>
2017-07-07 16:36:16 -0400

    v1.0

--- tasks/SvABA_maf/src/SvABA_maf.sh
+++ tasks/SvABA_maf/src/SvABA_maf.sh
@@ -1,11 +1,10 @@
 PID=$1
 TID=$2
 NID=$3
-VCF_SNV=$4  # Strelka SNV format
-VCF_INDEL=$5  # Strelka indel format
+VCF_INDEL=$4  # Strelka indel format
 export JULIA_LOAD_PATH="/opt/src"
-julia /opt/src/vcf2txt.jl $VCF_SNV  $PID.StrelkaSNV.tsv  
-julia /opt/src/vcf2txt.jl $VCF_INDEL  $PID.StrelkaINDEL.tsv
-julia /opt/src/Strelka_maflite.jl $TID $NID  $PID.StrelkaSNV.tsv $PID.StrelkaINDEL.tsv $PID.Strelka_maflite.tsv
+#sed 's/EVDNC.INDEL.//g'  $VCF_INDEL | sed 's/NDISC=.;//g' |sed 's/TDISC=.;//g'  | sed -E 's/PONCOUNT=[[:digit:]]+;//g' | sed -E 's/SCTG=c_[0-9]+_[0-9]+_[0-9]+_[0-9]+;//g' | grep -v '##FORMAT' > $PID.A.vcf
+julia /opt/src/vcf2txt.jl $VCF_INDEL  $PID.SvABA.INDEL.tsv
+julia /opt/src/SvABA_maflite.jl $TID $NID  $PID.SvABA.INDEL.tsv $PID.SvABA_maflite.tsv
 
 
