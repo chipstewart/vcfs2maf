a53b588b7b5bb54d22b02bd0f56425a03b003c45
Chip Stewart <stewart@broadinstitute.org>
2017-07-09 14:57:54 -0400

    fix

--- tasks/m2_maflite/src/m2_maflite.sh
+++ tasks/m2_maflite/src/m2_maflite.sh
@@ -4,6 +4,6 @@ PID=$3
 VCF1=$4
 export JULIA_LOAD_PATH="/opt/src"
 julia /opt/src/vcf2txt.jl $VCF1  tmp1.tsv
-julia /opt/src/M1M2maflite.jl $TID $NID tmp1.tsv $PID.m2_maflite.tsv
+julia /opt/src/m2_maflite.jl $TID $NID tmp1.tsv $PID.m2_maflite.tsv
 
 
