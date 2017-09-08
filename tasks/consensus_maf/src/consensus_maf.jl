13a32ca8afaec55d3de5ab5af0f067e1f59b01bb
stewart <stewart@broadinstitute.org>
2017-09-08 22:39:15 +0000

    consensus maf

--- tasks/consensus_maf/src/consensus_maf.jl
+++ tasks/consensus_maf/src/consensus_maf.jl
@@ -1,5 +1,7 @@
 #!/usr/local/bin/julia
-# ARGS=["REBC-AC8L-TP","REBC-AC8L-NT","sample.mutect.maflite.txt","REBC-AC8L-TP-NT.m2_maflite.tsv","REBC-AC8L-TP-NT.Strelka_maflite.tsv","REBC-AC8L-TP-NT.SvABA_maflite.tsv","M1","M2","STRELKA","SVABA","REBC-AC8L-TP-NT.merged_maflite.tsv"]
+# ARGS=["REBC-A90D-TP-NB-BI.intersection.maf","2","50","REBC-A90D-TP-NB-BI.consensus.maf"]
+# ARGS=["THCA-BJ-A45K-TP-NB.filter.intersection.maf","2","50","THCA-BJ-A45K-TP-NB.consensus.maf"]
+# ARGS=["/opt/test/REBC-A90K-TP-NB-BI.filter.intersection.maf","2","50","/opt/test/REBC-A90K-TP-NB-BI.consensus.maf"]
 using DataFrames
 in_maf_file=ARGS[1]
 NALG=ARGS[2]
@@ -12,32 +14,54 @@ isString(x::Number)=false
 isString(x::DataArrays.NAtype)=false
 isString(x::AbstractString)=true
 
-maflite_fields=["build","chr","start","end","ref_allele","tum_allele1","tum_allele2","tumor_barcode","normal_barcode","t_alt_count","t_ref_count","judgement","n_alt_count","n_ref_count","tumor_f"]
-maflite_symbols=map(x->Symbol(x),maflite_fields)
+if isString(NALG)
+    NALG = parse(Float64,NALG)
+end
+if isString(LONG)
+    LONG = parse(Float64,LONG)
+end
 
-
-if !isfile(file1)
+if !isfile(in_maf_file)
     println("missing input maf ", in_maf_file, " .")
 end
+
 df1 = readtable(in_maf_file,separator ='\t')
-if Symbol("_end") in names(df1)
-	rename!(df1, [:_end], [:end])
+
+nalgx=df1[:NALG]
+nalg=fill(0,length(nalgx))
+
+if all(map(x->isString(x),nalgx)) 
+    ni=0
+    for n1 in nalgx
+        println(ni,n1)
+        ni+=1
+        nalg[ni]=maximum(map(x->parse(Int32,x),split(n1,"|";limit=5)))        
+    end
+else
+    nalg=nalgx
 end
 
+delete!(df1, [:NALG])
+df1[:NALG]=nalg
+
 event_length =  map(x -> length(x),df1[:Reference_Allele])+map(x -> length(x),df1[:Tumor_Seq_Allele2])-1
-pass =  map(x -> x>=NALG,df1[:NALG]) | map(x -> x>=LONG),event_length)
+pass =  map(x -> x>=NALG,nalg) | map(x -> x>=LONG,event_length)
 
 for c in names(df1)
-	if ~isString(df1[1,c])
-		df1[c] = map(x -> string(x),df1[c])
-	end
+    if ~all(map(x->isString(x),df1[c]))
+        df1[c] = map(x -> string(x),df1[c])
+    end
+end
+
+for c in names(df1)
+    k=find(map(x -> x=="NA",df1[c]))
+    df1[k,c] = ""
 end
 
 k=find(pass)
-maf=df[k,:]
+maf=df1[k,:]
 
-open(consensus_maf_file, "w") do f
+open(out_maf_file, "w") do f
     writedlm(f, reshape(names(maf), 1, length(names(maf))), '\t')
     writedlm(f, convert(Array,maf), '\t')
 end
-
