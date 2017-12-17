d93e332aa12c6e2d83b254982dc52bb901261265
stewart <stewart@broadinstitute.org>
2017-12-17 18:08:41 +0000

    fix

--- tasks/merge_maflite/src/merge_maflite.sh
+++ tasks/merge_maflite/src/merge_maflite.sh
@@ -9,14 +9,14 @@ MAF3=$6  # Strelka
 MAF4=$7  # Snowman
 MAF5=$8  # SvABA 
 MAF6=$9  # Strelka2
-LAB1={$10}  # M1
+LAB1=${10}  # M1
 LAB2=${11}  # M2
 LAB3=${12}  # STRELKA
 LAB4=${13}  # Snowman
 LAB5=${14}  # SVABA
-LAB6=${16}  # SVABA
+LAB6=${15}  # Strelka2
 export JULIA_LOAD_PATH="/opt/src"
-echo "julia /opt/src/merge_maflite.jl $TID $NID $MAF1 $MAF2 $MAF3 $MAF4 $MAF5  $MAF6 $LAB1 $LAB2  $LAB3  $LAB4 $LAB5  $LAB6 $PID.merged.maflite.tsv"
-julia /opt/src/merge_maflite.jl $TID $NID $MAF1 $MAF2 $MAF3 $MAF4 $MAF5 $MAF6 $LAB1 $LAB2  $LAB3  $LAB4  $LAB5  $LAB6 $PID.merged.maflite.tsv
+echo "julia /opt/src/merge_maflite.jl $TID $NID $MAF1 $MAF2 $MAF3 $MAF4 $MAF5 $MAF6 $LAB1 $LAB2 $LAB3 $LAB4 $LAB5 $LAB6 $PID.merged.maflite.tsv"
+julia /opt/src/merge_maflite.jl $TID $NID $MAF1 $MAF2 $MAF3 $MAF4 $MAF5 $MAF6 $LAB1 $LAB2 $LAB3 $LAB4 $LAB5 $LAB6 $PID.merged.maflite.tsv
 
 
