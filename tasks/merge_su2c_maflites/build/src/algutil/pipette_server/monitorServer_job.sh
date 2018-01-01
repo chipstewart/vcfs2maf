921b35c8cd50aa8533fed807fbd813ddc82e1e06
stewart <stewart@broadinstitute.org>
2018-01-01 20:33:15 +0000

    delete

--- tasks/merge_su2c_maflites/build/src/algutil/pipette_server/monitorServer_job.sh
+++
@@ -1,2 +0,0 @@
-while true; do clear ; cat  /cga/fh/pcawg_pipeline/jobResults_pipette/status/report/job.status.txt|grep RUN; sleep 10 ; done
-
--- tasks/merge_su2c_maflites/build/src/algutil/pipette_server/monitorServer_pipeline.sh
+++
@@ -1,2 +0,0 @@
-
-while true; do clear ; date; cat /cga/fh/pcawg_pipeline/jobResults_pipette/status/report/pipeline_summary.status.txt; sleep 10 ; done
