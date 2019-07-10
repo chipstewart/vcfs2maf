2b1bdf9f645fdc23ea55569800361660d3472af9
Chip Stewart <donald.a.stewart@gmail.com>
2019-07-10 15:05:50 -0400

    julia 1.0, GATK4.1 M2, tidy

--- tasks/gatk4_m2_maflite/src/gatk4_m2_maflite.jl
+++ tasks/gatk4_m2_maflite/src/gatk4_m2_maflite.jl
@@ -1,13 +1,15 @@
 #!/usr/local/bin/julia
 # ARGS=["REBC-AC8L-TP","REBC-AC8L-NT","tmp1.tsv","REBC-AC8L-TP-NT.M2_maflite.tsv"]
 # ARGS=["SU2CLC-MGH-1047-TM-01","SU2CLC-MGH-1047-BL-01","tmp3.tsv","RP-1066_SU2CLC-MGH-1047"]
-# ARGS=["REBC-AF8C-NT1-A-1-1-D-A649-36","REBC-AF8C-TTP1-A-1-1-D-A649-36","tmp3.tsv","REBC-AF8C-NT-TP"]
+# ARGS=["REBC-AF8C-NT1-A-1-1-D-A649-36","REBC-AF8C-TTP1-A-1-1-D-A649-36","tmp3.tsv","REBC-AF8C-NT-TP","37"]
 
 using DataFrames, CSV, Statistics, DelimitedFiles
 tumor_id=ARGS[1]
 normal_id=ARGS[2]
 file1=ARGS[3]
 pair_id=ARGS[4]
+build_id=ARGS[5]
+
 file1a=pair_id*".raw.tsv"
 
 isString(x::Number)=false
@@ -28,7 +30,7 @@ FIELDS=names(df)
 FIELDX=[:x,:QUAL,:ID,:NORMAL_FT,:NORMAL_MMQ,:NORMAL_PID, :NORMAL_MBQ, :NORMAL_GQ, :NORMAL_OBF, :NORMAL_OBQRC,:NORMAL_AF,:NORMAL_FOXOG,
  :NORMAL_SA_MAP_AF, :NORMAL_MPOS, :NORMAL_PL, :NORMAL_OBAM, :NORMAL_OBQ, :NORMAL_ALT_F2R1, :NORMAL_SA_POST_PROB, :NORMAL_REF_F2R1,
  :NORMAL_GT, :NORMAL_MCL, :NORMAL_MFRL, :NORMAL_OBP, :NORMAL_OBAMRC, :NORMAL_PGT, :NORMAL_REF_F1R2,  :NORMAL_ALT_F1R2,
- :TUMOR_FT, :TUMOR_PID,  :TUMOR_GQ, :TUMOR_OBF, :TUMOR_OBQRC, :TUMOR_SA_MAP_AF, :TUMOR_PL, :TUMOR_OBAM,
+ :TUMOR_FT, :TUMOR_PID,  :TUMOR_GQ, :TUMOR_OBF, :TUMOR_OBQRC, :TUMOR_SA_MAP_AF, :TUMOR_PL, :TUMOR_OBAM, :NORMAL_SB, 
  :TUMOR_OBQ, :TUMOR_SA_POST_PROB,:TUMOR_GT,:TUMOR_OBP, :TUMOR_OBAMRCM, :TUMOR_PGT, :TUMOR_SB, :OCM, :MMQ, :MBQ, :SEQQ,:STRQ, :MPOS, :RPA,:GERMQ, 
  :MFRL, :CONTQ, :ECNT,:NALOD, :STRANDQ, :TUMOR_PS, :NORMAL_PS, :NORMAL_F2R1_REF, :NORMAL_F2R1_ALT, :NORMAL_F1R2_REF, :NORMAL_F1R2_ALT]
 
@@ -158,11 +160,18 @@ for c in names(df)
     end
 end
 
-# maflite fields 
-# SNP: build    chr start   end ref_allele  tum_allele1 tum_allele2 tumor_barcode   normal_barcode  tumor_f init_t_lod  t_lod_fstar t_alt_count t_ref_count judgement
-# INDEL: build  contig  start_position  end_position    ref_allele  alt_allele1 alt_allele2 tumor_name  normal_name tumor_f n_ref_count n_alt_count t_ref_count t_alt_count init_n_lod  init_t_lod  judgement
+# multi-alt allele events 
+k=findall(map(x-> length(split(x,':'))>1, df[:ALT]))
+df[k,:ALT]=map(x-> split(x,':')[1], df[k,:ALT])
+df[k,:POPAF]=map(x-> split(x,':')[1], df[k,:POPAF])
+df[k,:NLOD]=map(x-> split(x,':')[1], df[k,:NLOD])
+df[k,:TLOD]=map(x-> split(x,':')[1], df[k,:TLOD])
+df[k,:TUMOR_AF]=map(x-> split(x,':')[1], df[k,:TUMOR_AF])
+
+
 
 df[:end]=df[:POS]
+
 
 # start for indels is POS+1
 kdel = map(x-> length(x)>1, df[:REF]).*df[:INDEL]
@@ -204,12 +213,17 @@ kmnp = map(x-> length(x)>1, df[:ALT]).*map(x->!x,df[:INDEL])
 k = findall(kmnp)
 ref=df[k,:REF]
 dref = map(x-> length(x), ref)
-df[k,:end]=df[k,:POS]+dref-.1
+df[k,:end]=df[k,:POS]+dref.-1
 
+
+
+# maflite fields 
+# SNP: build    chr start   end ref_allele  tum_allele1 tum_allele2 tumor_barcode   normal_barcode  tumor_f init_t_lod  t_lod_fstar t_alt_count t_ref_count judgement
+# INDEL: build  contig  start_position  end_position    ref_allele  alt_allele1 alt_allele2 tumor_name  normal_name tumor_f n_ref_count n_alt_count t_ref_count t_alt_count init_n_lod  init_t_lod  judgement
 
 
 # add build column
-df[:build]=fill("37",size(df,1))
+df[:build]=fill(build_id,size(df,1))
 df[:tumor_barcode]=fill(tumor_id,size(df,1))
 df[:normal_barcode]=fill(normal_id,size(df,1))
 df[:judgement]=fill("KEEP",size(df,1))
@@ -218,7 +232,7 @@ df[:judgement]=fill("KEEP",size(df,1))
 # allele counts
 df[:n_alt_count]=df[:NORMAL_AD_ALT]
 df[:n_ref_count]=df[:NORMAL_AD_REF]
-df[:t_lod_fstar]=df[:TLOD]
+#$df[:t_lod_fstar]=df[:TLOD]
 df[:tumor_f]=df[:TUMOR_AF]
 df[:t_alt_count]=df[:TUMOR_AD_ALT]
 df[:t_ref_count]=df[:TUMOR_AD_REF]
@@ -234,6 +248,14 @@ rename!(df, [:CHRO,:POS, :REF, :ALT], [:chr, :start, :ref_allele,:alt_allele])#f
 #        rename!(df,c,m2c)
 #    end
 #end
+FIELDS=names(df)
+FIELDX=[:NORMAL_DP, :NORMAL_AD_REF, :NORMAL_AD_ALT, :INDEL, :Variant_Type, :ALT0, 
+ :TUMOR_DP, :TUMOR_AF, :TUMOR_AD_REF, :TUMOR_AD_ALT, :DP]
+for i=1:length(FIELDX)
+    if (FIELDX[i] in FIELDS)
+        deletecols!(df, FIELDX[i])
+    end
+end
 
 maf=df
 for c in names(maf)
@@ -246,23 +268,23 @@ end
 
 for c in names(maf)
     println(c)
-    k=isna(maf[c])
+    k=map(x->ismissing(x),df[c]) 
     if mean(k)==1.0
         delete!(maf, c)
         continue
     end
-    k=find(map(x->x=="NA",maf[c]))
+    k=findall(map(x->x=="NA",maf[c]))
     maf[k,c] = ""
-    k=find(map(x->uppercase(x)=="TRUE",maf[c]))
+    k=findall(map(x->uppercase(x)=="TRUE",maf[c]))
     maf[k,c] = "1"
-    k=find(map(x->uppercase(x)=="FALSE",maf[c]))
+    k=findall(map(x->uppercase(x)=="FALSE",maf[c]))
     maf[k,c] = "0"
 end
 
 maflite_all=pair_id*".m2.all.maflite.tsv"
 open(maflite_all, "w") do f
     writedlm(f, reshape(names(maf), 1, length(names(maf))), '\t')
-    writedlm(f, convert(Array,maf), '\t')
+    writedlm(f, convert(Matrix,maf), '\t')
 end
 
 kpass = find(map(x-> uppercase(x)=="PASS", maf[:FILTER]))
@@ -273,6 +295,7 @@ delete!(maf,[:FILTER])
 maflite_pass=pair_id*".m2.pass.maflite.tsv"
 open(maflite_pass, "w") do f
     writedlm(f, reshape(names(maf), 1, length(names(maf))), '\t')
-    writedlm(f, convert(Array,maf), '\t')
+    writedlm(f, convert(Matrix,maf), '\t')
 end
+
 
