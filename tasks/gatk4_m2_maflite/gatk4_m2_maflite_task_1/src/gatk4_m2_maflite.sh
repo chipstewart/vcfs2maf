456661ef2cdacceabd3d797facde890742de2944
Chip Stewart <stewart@broadinsitute.org>
2019-07-16 14:13:41 +0000

    fixes...

--- tasks/gatk4_m2_maflite/gatk4_m2_maflite_task_1/src/gatk4_m2_maflite.sh
+++ tasks/gatk4_m2_maflite/gatk4_m2_maflite_task_1/src/gatk4_m2_maflite.sh
@@ -4,28 +4,41 @@ PID=$3
 VCF1=$4
 BUILD=$5
 DETIN_FILT=$6
-export JULIA_LOAD_PATH="/opt/src"
+
+set -x
+
+echo $TID
+echo $NID
+echo $PID
+echo $VCF1
+echo $BUILD
+echo $DETIN_FILT
+
+export JULIA_LOAD_PATH=":.:/opt/src"
+which julia
 
 julia /opt/src/vcf2txt.jl $VCF1  tmp1.tsv
 sed "s/${TID}/TUMOR/g" tmp1.tsv > tmp2.tsv
 sed "s/${NID}/NORMAL/g" tmp2.tsv > tmp3.tsv
 
 
-julia /opt/src/gatk4_m2_maflite.jl $TID $NID tmp3.tsv $PID
+julia /opt/src/gatk4_m2_maflite.jl $TID $NID tmp3.tsv $PID $BUILD
 
 
-filts=$(echo $DETIN_FILT | tr ";" "\n")
+filts=$(echo $DETIN_FILT | tr "," "\n")
 
-cat $PID.m2.pass.maflite.tsv > tmp1.tsv
+cp $PID.m2.all.maflite.tsv  tmpA.tsv
 
 for filt in $filts
 do
-    echo "> [$filt]"
-    grep -v $filt1 tmp1.tsv tmp2.tsv
-    cat tmp2.tsv > tmp1.tsv
+	echo " [$filt]"
+	grep -v $filt tmpA.tsv > tmpB.tsv
+	cp tmpB.tsv  tmpA.tsv
+	wc -l tmpA.tsv
 done
 
-cat tmp1.tsv > $PID.m2.deTiN.maflite.tsv 
+cp tmpA.tsv $PID.m2.deTiN.maflite.tsv 
+
 
 
 
